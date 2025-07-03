e35250de68e44d058d90b11e4cefa70dcc947de9
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-07-03 12:41:32 +0200

    fix: moved method to match git diff

--- clients/java/src/main/java/io/camunda/client/impl/CamundaClientImpl.java
+++ clients/java/src/main/java/io/camunda/client/impl/CamundaClientImpl.java
@@ -1246,11 +1246,6 @@ public final class CamundaClientImpl implements CamundaClient {
     return new JobSearchRequestImpl(httpClient, jsonMapper);
   }
 
-  private JobClient newJobClient() {
-    return new JobClientImpl(
-        asyncStub, httpClient, config, jsonMapper, credentialsProvider::shouldRetryRequest);
-  }
-
   @Override
   public MessageSubscriptionSearchRequest newMessageSubscriptionSearchRequest() {
     return new MessageSubscriptionSearchRequestImpl(httpClient, jsonMapper);
@@ -1347,5 +1342,10 @@ public final class CamundaClientImpl implements CamundaClient {
   public DeleteDocumentCommandStep1 newDeleteDocumentCommand(
       final DocumentReferenceResponse documentReference) {
     return jobClient.newDeleteDocumentCommand(documentReference);
+  }
+
+  private JobClient newJobClient() {
+    return new JobClientImpl(
+        asyncStub, httpClient, config, jsonMapper, credentialsProvider::shouldRetryRequest);
   }
 }
