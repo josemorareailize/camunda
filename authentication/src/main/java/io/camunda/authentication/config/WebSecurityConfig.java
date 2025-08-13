b931a6df7c624f30f834700069b8436d95356ba0
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-08-13 23:44:49 +0200

    feat: make refresh interval configurable

--- authentication/src/main/java/io/camunda/authentication/config/WebSecurityConfig.java
+++ authentication/src/main/java/io/camunda/authentication/config/WebSecurityConfig.java
@@ -517,7 +517,8 @@ public class WebSecurityConfig {
                           .authenticationEntryPoint(authFailureHandler)
                           .accessDeniedHandler(authFailureHandler))
               .addFilterAfter(
-                  new SessionAuthenticationRefreshFilter(authenticationProvider),
+                  new SessionAuthenticationRefreshFilter(
+                      authenticationProvider, securityConfiguration.getAuthentication()),
                   SecurityContextHolderFilter.class)
               .addFilterAfter(
                   new WebApplicationAuthorizationCheckFilter(
@@ -736,7 +737,8 @@ public class WebSecurityConfig {
                           .logoutSuccessHandler(new NoContentResponseHandler())
                           .deleteCookies(SESSION_COOKIE, X_CSRF_TOKEN))
               .addFilterAfter(
-                  new SessionAuthenticationRefreshFilter(authenticationProvider),
+                  new SessionAuthenticationRefreshFilter(
+                      authenticationProvider, securityConfiguration.getAuthentication()),
                   SecurityContextHolderFilter.class)
               .addFilterAfter(
                   new WebApplicationAuthorizationCheckFilter(
